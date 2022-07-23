1e806b572955fbbdd51d1c2906bfbe1a466c0310
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-07-23 18:12:08 +0500

    feat: add header

---
+++ src/components/StatisticsItem.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps } from "vue";
+import ChartIcon from "../icons/chart-bar-chart-svgrepo-com.svg";
+const props = defineProps({
+    data: {
+        type: Array, 
+        required: true,
+    }
+})
+
+</script>
+
+<template>
+    <li class="flex items-center justify-between">
+        <div>
+            <p class="font-semibold capitalize text-[11px] text-darkgray mb-2">{{ props.data.statsTitle }}</p>
+            <div class="flex justify-between">
+                <span>{{ props.data.numberOfTasks }}</span>
+                <span> {{ props.data.amountOfIncreasing }}</span>
+            </div>
+            <p>{{ props.data.dateStatus }}</p>
+        </div>
+        <ChartIcon class="h-[60px] w-[60px] stroke-current" />
+    </li>
+</template>
---
+++ src/components/StatisticsList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { reactive } from "vue";
+import StatisticsItem from "./StatisticsItem.vue";
+const statsList = reactive([
+    {   
+        id: 1,
+        statsTitle: "Tasks in Progress", 
+        numberOfTasks: '210',
+        amountOfIncreasing: "23",
+        dateStatus: "Current month",
+    },
+    {   
+        id: 2,
+        statsTitle: "Tasks Completed", 
+        numberOfTasks: '185',
+        amountOfIncreasing: "45",
+        dateStatus: "Current month",
+    },
+    {   
+        id: 3,
+        statsTitle: "Monthly Tasks Summary", 
+        numberOfTasks: '1,240',
+        amountOfIncreasing: "145",
+        dateStatus: "Current month",
+    },
+])
+</script>
+
+<template>
+    <ul class="grid grid-cols-3 gap-x-10">
+        <StatisticsItem v-for="item in statsList" :data="item" :key="item.id"/>
+    </ul>
+</template>
